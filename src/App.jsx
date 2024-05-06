59999d3141af4efb52941e389f97ec34da8dfe19
Akshay <mail.akshayks@gmail.com>
2024-05-06 21:15:22 +0530

    added basic homepage skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        <HomePage />
+      </section>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <main className="flex-1 p-4 flex flex-col justify-center"></main>;
+}
+
+export default HomePage;
