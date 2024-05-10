583980fa897646eebe1e4a0ab198bd4521c3943d
Akshay <mail.akshayks@gmail.com>
2024-05-11 00:05:33 +0530

    added transcribing animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import FileDisplay from "./components/FileDisplay";
+import Information from "./components/Information";
+import Transcribing from "./components/Transcribing";
 
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
+  const [output, setOutput] = useState(null);
+  const [downloading, setDownloading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [finished, setFinished] = useState(false);
 
   const isAudioAvailable = file || audioStream;
 
@@ -14,21 +20,28 @@ function App() {
     setAudioStream(null);
   }
 
+  async function handleFormSubmission() {}
+
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
-        {isAudioAvailable ? (
+        {output ? (
+          <Information output={output} finished={finished} />
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailable ? (
           <FileDisplay
+            handleFormSubmission={handleFormSubmission}
+            handleAudioReset={handleAudioReset}
             file={file}
             audioStream={audioStream}
-            handleAudioReset={handleAudioReset}
-            handleFormSubmission={() => {}}
           />
         ) : (
           <HomePage setFile={setFile} setAudioStream={setAudioStream} />
         )}
       </section>
+      <footer></footer>
     </div>
   );
 }
